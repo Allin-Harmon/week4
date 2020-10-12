15ab1cf3361ef8a4ac93bfac8cfdb22cd4df3a68
unknown <allinharmon@gmail.com>
2020-10-12 14:16:41 -0400

    Week 4 Homework 1

---
+++ app1.js
@@ -0,0 +1,45 @@
+console.log('Hello world!');//step 6
+console.warn('This is a warning!');//step 7
+console.error('Error! System critical!');// step 8
+
+var var1 = 'String';//step 9 and 10
+
+console.log(var1);//step 11
+console.log('\n');//step 12
+console.log(var1);//shows the blank line
+
+var var2 = 4;//step 13
+
+console.log(var1 + var2);//step 14
+//step 15 - guess is String4
+
+/* A way to multi line comment
+Some types covered in class today
+strings, numbers, boolean, undefined
+--step 16a */
+
+/**
+ * An alternate way to multi line comment used for more important info
+ * Some types covered in class today
+ * strings, numbers, boolean, undefined
+ * 
+ * --step 16b 
+ * 
+ */
+
+console.log(typeof var1);//step 17
+//step 18 - guess is string
+
+var var3 = 'text';//step 19
+
+console.log(var3);//step 20
+
+var3 = true;//step 20
+
+console.log(var3);//step 21
+
+var1 = 5;//step 22
+var2 = '5';//step 23
+
+console.log (var1 + Number(var2));
+
