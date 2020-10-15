3c248ce110934f9a7373f59d3245cd4b4523371c
HBull5 <harcourtbullv@gmail.com>
2020-10-15 17:26:49 -0400

    feedback not finished

--- app2.js
+++ app2.js
@@ -1,33 +1,35 @@
-// step 1-4 set up 
+// step 1-4 set up
 
-var num1 = prompt("Enter a number"); //step 5a 
-console.log(num1); //step 5b
+// var num1 = prompt('Enter a number'); //step 5a
+// console.log(num1); //step 5b
 
-var num2 = prompt("Enter another number"); //step 6a 
-console.log(num2); //step 6b
+// var num2 = prompt('Enter another number'); //step 6a
+// console.log(num2); //step 6b
 
-num3 = Math.pow(Number(num1), Number(num2));
-alert(num3); //step 7
+// num3 = Math.pow(Number(num1), Number(num2));
+// alert(num3); //step 7
+// alert(num1 ** num2); //? Just another way to do the same thing
 
-console.log(Math.floor(Math.random() * 3) + 1);//step 8
+console.log(Math.floor(Math.random() * 3) + 1); //step 8
 
 var arr = ['a', 'b', 'c', 'd', 'e', 'f']; //step 9
 arr.pop('f'); //step 10       | or arr.pop([5]);
-console.log(arr);
+// console.log(arr);
 
 arr.push('g'); //step 11
 // console.log(arr);
 
-arr.shift('a'); //step 12
+// arr.shift('a'); //step 12
+arr.shift();
 // console.log(arr);
 
 arr.unshift(1); //step 13
 console.log(arr); //step 14
 
-arr.splice(2, 3, ['cat', 'dog', 'rabbit']);//step 15  or did you mean this arr.splice(2, 3, 'cat', 'dog', 'rabbit');?
-// console.log(arr); 
+arr.splice(2, 3, ['cat', 'dog', 'rabbit']); //step 15  or did you mean this arr.splice(2, 3, 'cat', 'dog', 'rabbit');?
+// console.log(arr);
 
-var copied = arr.slice(0, 5);//step 16
+var copied = arr.slice(0, 5); //step 16
 // console.log(copied);
 
-console.log(copied[2][1]);//step 17
+console.log(copied[2][1]); //step 17
